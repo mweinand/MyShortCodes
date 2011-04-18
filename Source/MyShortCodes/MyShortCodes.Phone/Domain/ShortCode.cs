1952777a923cf4b8f90193ab22017620f69e9f22
Michael Weinand <mweinand@proace.com>
2011-04-18 18:46:29 -0500

    start of project

---
+++ Source/MyShortCodes/MyShortCodes.Phone/Domain/ShortCode.cs
@@ -0,0 +1,9 @@
+namespace MyShortCodes.Phone.Domain
+{
+    public class ShortCode
+    {
+        public int ShortCodeId { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+}
